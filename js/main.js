642c266e69a8859a54018c2fc112c3d3760f5eb8
Jason You <jasonyou92@gmail.com>
2017-09-22 15:41:01 -0700

    javascript changes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-var today = new Date();
-var currentHour = document.getElementById('hours');
-var currentMinute = document.getElementById('minutes');
-var currentSecond = document.getElementById('seconds');
-var hour = today.getHours();
-var minute = today.getMinutes();
-var second = today.getSeconds();
-var hourText = document.createTextNode(hour);
-var minuteText = document.createTextNode(minute);
-var secondText = document.createTextNode(second);
-currentHour.appendChild(hourText);
-currentMinute.appendChild(minuteText);
-currentSecond.appendChild(secondText);
+function startTime(){
+	var today = new Date();
+	var currentHour = document.getElementById('hours');
+	var currentMinute = document.getElementById('minutes');
+	var currentSecond = document.getElementById('seconds');
+	var hour = today.getHours();
+	var minute = today.getMinutes();
+	var second = today.getSeconds();
+	if (hour > 12) {
+		hour = hour - 12;
+	}
+	if (minute < 10) {
+		minute = "0" + minute;
+	}
+	if (second < 10) {
+		second = "0" + second;
+	}
+	document.getElementById('time').textContent = hour + " : " + minute + " : " + second;
+	setTimeout(function() {
+		startTime()
+	}, 1000);
+}
+
+document.getElementById('time').addEventListener("onload", startTime(), false);
